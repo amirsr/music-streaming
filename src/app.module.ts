f08cb450e439024b52b9b95c6a993c45622c2c9c
amirsr <amir@base.la>
2022-12-18 10:52:01 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://EightBasson:wrZzz5ml7hoT8Uf4@cluster1.gfmjqjx.mongodb.net/?retryWrites=true&w=majority',
+      'xxxxxxx',
     ),
     AuthModule,
   ],
